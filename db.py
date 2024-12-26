9ebe9039bcea649d0ce09b0c16705f7c586b8124
R4JV4N5 <rajvenniyar65@gmail.com>
2024-12-26 18:31:22 +0530

    changes in files in docker and variables

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-username = 'root'
-password = 'root'
-host = 'localhost'
-database = 'reportai_database'
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')  # Update the host to allow Docker to access local machine
+database = os.getenv('DB')
 
 # Construct the connection string
 connection_string = f'mysql+mysqlconnector://{username}:{password}@{host}/{database}'
@@ -24,9 +27,9 @@ def get_db():
         db.close()
 
 # Test the connection
-# try:
-#     with engine.connect() as connection:
-#         result = connection.execute(text("select * from users"))  # Use `text` for raw SQL queries
-#         print("Connection successful:", result.fetchall())
-# except Exception as e:
-#     print("Connection failed:", str(e))
+try:
+    with engine.connect() as connection:
+        result = connection.execute(text("select count(UserID) from users"))  # Use `text` for raw SQL queries
+        print("Connection successful:", result.fetchall())
+except Exception as e:
+    print("Connection failed:", str(e))
